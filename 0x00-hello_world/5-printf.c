c311212a3cae185c41f4949d4b5478cd6bda4ef8
ChrisMario Ipeh <chrismariolipehunim@gmail.com>
2023-08-10 14:39:22 -0700

    Programming is like building

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,9 @@
 #include<stdio.h>
-
 /**
-
 * main - Entry point
-
-*
-
 * Return: Always 0 (Success)
-
 */
-
 int main(void)
-
 {
 
 printf("with proper grammar, but the outcome is a piece of art,\n");
@@ -19,5 +11,3 @@ printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
-
-
