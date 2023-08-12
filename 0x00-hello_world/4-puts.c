3b2967622e69ae885eed3a44c1438eeba5c20f49
ChrisMario Ipeh <chrismariolipehunim@gmail.com>
2023-08-12 15:45:11 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
-
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - this is the main function
+ *
+ * Return: always 0
  */
 
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 
 }
-
-
