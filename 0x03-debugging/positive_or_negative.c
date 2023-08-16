65b018aee8dcc0268842f08b59b92987daf50fdf
ChrisMario Ipeh <chrismariolipehunim@gmail.com>
2023-08-16 13:12:29 -0700

    task 0 debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * positive_or_negative - text 1
+ * @n: arguments
+ */
+
+void positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+}
