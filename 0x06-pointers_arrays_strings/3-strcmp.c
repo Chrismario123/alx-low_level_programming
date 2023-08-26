add6a458f9764074d340e22da97519a5da57b570
ChrisMario Ipeh <chrismariolipehunim@gmail.com>
2023-08-26 04:56:47 -0700

    task 3 of more pointers..

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
+	int x;
 
-i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if (s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
-}
-i++;
-}
-return (0);
+	for (x = 0 ; s1[x] != '\0' || s2[x] != '\0' ; x++)
+	{
+		if (s1[x] != s2[x])
+		{
+			if (s1[x] < s2[x])
+				return (s1[x] - s2[x]);
+			else if (s2[x] < s1[x])
+				return (s1[x] - s2[x]);
+		}
+	}
+	return (0);
 }
