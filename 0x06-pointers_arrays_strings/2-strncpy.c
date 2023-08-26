be3f98503b087448e12ae425e671428f01d7e1b3
ChrisMario Ipeh <chrismariolipehunim@gmail.com>
2023-08-26 04:08:10 -0700

    task 2 of more pointers..

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
+	int i;
 
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[j] = src[j];
-j++;
-}
-while (j < n)
-{
-dest[j] = '\0';
-j++;
-}
-return (dest);
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
