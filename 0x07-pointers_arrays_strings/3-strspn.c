074ea69dd1388e3715eadff1c2a40f281749d297
ChrisMArio Ipeh <chrismariolipehunim@gmail.com>
2023-08-29 15:46:34 +0100

    task 3 of even more pointers..

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,15 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int n = 0;
-int r;
+	int i, p;
 
-while (*s)
-{
-for (r = 0; accept[r]; r++)
-{
-if (*s == accept[r])
-{
-n++;
-break;
-}
-else if (accept[r + 1] == '\0')
-return (n);
-}
-s++;
-}
-return (n);
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (p = 0 ; s[i] != accept[p] ; p++)
+		{
+			if (accept[p] == '\0')
+				return (i);
+		}
+	}
+	return (0);
 }
