e4f2d813d834333a925cff81cfe94c4cbed937cb
ChrisMArio Ipeh <chrismariolipehunim@gmail.com>
2023-09-06 16:18:16 +0100

    task 1 of malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,18 +10,21 @@
 
 char *_strdup(char *str)
 {
-char *aaa;
-int i, r = 0;
+	char *abs;
+	int j;
+       int k = 0;
 
-if (str == NULL)
-return (NULL);
-i = 0;
-while (str[i] != '\0')
-i++;
-aaa = malloc(sizeof(char) * (i + 1));
-if (aaa == NULL)
-return (NULL);
-for (r = 0; str[r]; r++)
-aaa[r] = str[r];
-return (aaa);
+	if (str == NULL)
+		return (NULL);
+	for (j = 0 ; str[j] !=  '\0' ; j++)
+		k++;
+	abs = malloc(sizeof(char) * (k + 1));
+			if (abs == NULL)
+			return (NULL);
+	else
+	for (j = 0 ; str[j] != '\0' ; j++)
+
+		abs[j] = str[j];
+
+	return (abs);
 }
